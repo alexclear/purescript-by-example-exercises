f7124349cd7f29c0bcede1338dbcc9bc7d7b26cc
Alex Chistyakov <alexclear@gmail.com>
2018-12-09 17:42:21 +0300

    Add numEvens

--- 04_recursion_maps_and_folds/src/PBE/RecursionOnArrays.purs
+++ 04_recursion_maps_and_folds/src/PBE/RecursionOnArrays.purs
@@ -2,9 +2,17 @@ module PBE.RecursionOnArrays where
 
 import Prelude
 import Data.Array (null)
-import Data.Array.Partial (tail)
+import Data.Array.Partial (tail, head)
 import Partial.Unsafe (unsafePartial)
 
 isEven :: Int -> Boolean
 isEven 0 = true
 isEven n = not isEven (n - 1)
+
+numEvens :: Array Int -> Int
+numEvens arr =
+  if null arr
+    then 0
+    else if isEven (unsafePartial head arr)
+      then 1 + numEvens (unsafePartial tail arr)
+      else numEvens (unsafePartial tail arr)
